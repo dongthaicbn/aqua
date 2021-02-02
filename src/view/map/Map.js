3c116fa636c1502d2d1a5714c8973dca6f3285b7
hung.cv <hungcv.it@tripi.vn>
2021-02-02 14:12:41 +0700

    implement map

--- src/view/map/Map.js
+++ src/view/map/Map.js
@@ -1,96 +1,18 @@
 import React from 'react';
-import {
-  GoogleMap,
-  withGoogleMap,
-  withScriptjs,
-  OverlayView,
-} from 'react-google-maps';
-import { compose, withProps } from 'recompose';
+import MapGoogle from './components/MapGoogle';
 
-export const KEY_GOOGLE_MAP = 'AIzaSyA9SPYP838bh5o7pBC8xA7632sDJ0jJwxM';
-const BLUE = '#00B6F3';
-const WHITE = '#FFFFFF';
-
-const Map = compose(
-  withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${KEY_GOOGLE_MAP}&v=3.exp&libraries=geometry,drawing,places`,
-    loadingElement: <div style={{ width: '100%', height: '100%' }} />,
-    containerElement: <div style={{ width: '100%', height: '100%' }} />,
-    mapElement: <div style={{ width: '100%', height: '100%' }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)((props) => {
-  const items = [{ latitude: 21.00626, longitude: 105.85537, id: 1 }];
-  const coordinateActive = { latitude: 21.00626, longitude: 105.85537 };
-  const [zoom] = React.useState(12);
-
-  const disableOption = {
-    mapTypeControl: false,
-    fullscreenControl: false,
-    clickableIcons: true,
-    streetViewControl: true,
-    zoomControl: true,
-    keyboardShortcuts: true,
-    draggableCursor: 'pointer',
-    // gestureHandling: "none",
-  };
-  const getPixelPositionOffset = (width, height) => ({
-    x: -(width / 2),
-    y: -(height / 2),
-  });
+const Map = (props) => {
   return (
-    <div style={{ height: 500, width: '100%' }}>
-      <GoogleMap
-        defaultZoom={zoom}
-        center={{
-          lat: coordinateActive.latitude,
-          lng: coordinateActive.longitude,
-        }}
-        key={KEY_GOOGLE_MAP}
-        defaultOptions={disableOption}
-      >
-        {items.map((el) => {
-          const isActive = true;
-          return (
-            <OverlayView
-              key={el.id}
-              position={{ lat: el.latitude, lng: el.longitude }}
-              mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-              getPixelPositionOffset={getPixelPositionOffset}
-            >
-              <div
-                style={{
-                  backgroundColor: isActive ? BLUE : WHITE,
-                  fontSize: 14,
-                  lineHeight: '17px',
-                  padding: '3px 6px',
-                  borderRadius: 8,
-                  position: 'relative',
-                  color: isActive ? WHITE : BLUE,
-                }}
-                onClick={() => {}}
-                aria-hidden="true"
-              >
-                <div>aaaa</div>
-                <div
-                  style={{
-                    width: 9,
-                    height: 9,
-                    backgroundColor: isActive ? BLUE : WHITE,
-                    transform: 'rotate(45deg)',
-                    position: 'absolute',
-                    left: 'calc(50% - 5px)',
-                    marginTop: -1,
-                  }}
-                />
-              </div>
-            </OverlayView>
-          );
-        })}
-      </GoogleMap>
+    <div
+      style={{
+        height: 'calc(100vh - 64px)',
+        width: '100%',
+        background: '#F5F5F5',
+      }}
+    >
+      <MapGoogle />
     </div>
   );
-});
+};
 
 export default Map;
---
+++ src/view/map/components/MapGoogle.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  GoogleMap,
+  withGoogleMap,
+  withScriptjs,
+  OverlayView,
+} from 'react-google-maps';
+import { compose, withProps } from 'recompose';
+
+export const KEY_GOOGLE_MAP = 'AIzaSyA9SPYP838bh5o7pBC8xA7632sDJ0jJwxM';
+const BLUE = '#00B6F3';
+const WHITE = '#FFFFFF';
+
+const MapGoogle = compose(
+  withProps({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${KEY_GOOGLE_MAP}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ width: '100%', height: '100%' }} />,
+    containerElement: <div style={{ width: '100%', height: '100%' }} />,
+    mapElement: <div style={{ width: '100%', height: '100%' }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => {
+  const items = [{ latitude: 21.00626, longitude: 105.85537, id: 1 }];
+  const coordinateActive = { latitude: 21.00626, longitude: 105.85537 };
+  const [zoom] = React.useState(12);
+
+  const disableOption = {
+    mapTypeControl: false,
+    fullscreenControl: false,
+    clickableIcons: true,
+    streetViewControl: true,
+    zoomControl: true,
+    keyboardShortcuts: true,
+    draggableCursor: 'pointer',
+    // gestureHandling: "none",
+  };
+  const getPixelPositionOffset = (width, height) => ({
+    x: -(width / 2),
+    y: -(height / 2),
+  });
+  return (
+    <GoogleMap
+      defaultZoom={zoom}
+      center={{
+        lat: coordinateActive.latitude,
+        lng: coordinateActive.longitude,
+      }}
+      key={KEY_GOOGLE_MAP}
+      defaultOptions={disableOption}
+    >
+      {(items || []).map((el) => {
+        const isActive = el.id === 1;
+        return (
+          <OverlayView
+            key={el.id}
+            position={{ lat: el.latitude, lng: el.longitude }}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+            getPixelPositionOffset={getPixelPositionOffset}
+          >
+            <div
+              style={{
+                backgroundColor: isActive ? BLUE : WHITE,
+                fontSize: 14,
+                lineHeight: '17px',
+                padding: '3px 6px',
+                borderRadius: 8,
+                position: 'relative',
+                color: isActive ? WHITE : BLUE,
+              }}
+              onClick={() => {}}
+              aria-hidden="true"
+            >
+              <div>abc</div>
+              <div
+                style={{
+                  width: 9,
+                  height: 9,
+                  backgroundColor: isActive ? BLUE : WHITE,
+                  transform: 'rotate(45deg)',
+                  position: 'absolute',
+                  left: 'calc(50% - 5px)',
+                  marginTop: -1,
+                }}
+              />
+            </div>
+          </OverlayView>
+        );
+      })}
+    </GoogleMap>
+  );
+});
+
+export default MapGoogle;
