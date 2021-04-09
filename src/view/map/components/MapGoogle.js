00ad1799de7af9427b4536095f27908c77251d0e
hung.cv <hungcv.it@tripi.vn>
2021-04-09 16:27:40 +0700

    fix bug

--- src/view/map/components/MapGoogle.js
+++ src/view/map/components/MapGoogle.js
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Radio } from 'antd';
-import moment from 'moment';
+import React, { useEffect, useState } from "react";
+import { Button, Radio } from "antd";
+import moment from "moment";
 import {
   GoogleMap,
   withGoogleMap,
   withScriptjs,
   OverlayView,
-} from 'react-google-maps';
-import { HistoryOutlined } from '@ant-design/icons';
-import { compose, withProps } from 'recompose';
-import { isEmpty } from 'utils/helpers/helpers';
-import ViewDeviceModal from './ViewDeviceModal';
+  InfoWindow,
+} from "react-google-maps";
+import { HistoryOutlined } from "@ant-design/icons";
+import { compose, withProps } from "recompose";
+import { isEmpty } from "utils/helpers/helpers";
+import ViewDeviceModal from "./ViewDeviceModal";
 
 // export const KEY_GOOGLE_MAP = 'AIzaSyA9SPYP838bh5o7pBC8xA7632sDJ0jJwxM';
-export const KEY_GOOGLE_MAP = 'AIzaSyDemuTXbh1ONO1hYzbfP-TGCkPRI2jwaPA';
+export const KEY_GOOGLE_MAP = "AIzaSyDemuTXbh1ONO1hYzbfP-TGCkPRI2jwaPA";
 
 // const BLUE = '#00B6F3';
-const WHITE = '#FFFFFF';
+const WHITE = "#FFFFFF";
 let device_id = null;
 
 const MapGoogle = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${KEY_GOOGLE_MAP}&v=3.exp&libraries=geometry,drawing,places`,
-    loadingElement: <div style={{ width: '100%', height: '100%' }} />,
-    containerElement: <div style={{ width: '100%', height: '100%' }} />,
-    mapElement: <div style={{ width: '100%', height: '100%' }} />,
+    loadingElement: <div style={{ width: "100%", height: "100%" }} />,
+    containerElement: <div style={{ width: "100%", height: "100%" }} />,
+    mapElement: <div style={{ width: "100%", height: "100%" }} />,
   }),
   withScriptjs,
   withGoogleMap
@@ -54,7 +55,7 @@ const MapGoogle = compose(
     streetViewControl: true,
     zoomControl: true,
     keyboardShortcuts: true,
-    draggableCursor: 'pointer',
+    draggableCursor: "pointer",
     // gestureHandling: "none",
   };
   const getPixelPositionOffset = (width, height) => ({
@@ -90,6 +91,15 @@ const MapGoogle = compose(
             if (isEmpty(el)) return null;
             return (
               <>
+                {/* <Marker
+                  key={i}
+                  position={{ lat: el.latitude, lng: el.longitude }}
+                  onClick={() => {}}
+                >
+                  <InfoWindow onCloseClick={() => {}}>
+                    <p>aaaa</p>
+                  </InfoWindow>
+                </Marker> */}
                 <OverlayView
                   key={el.device_id}
                   position={{ lat: el.latitude, lng: el.longitude }}
@@ -99,14 +109,14 @@ const MapGoogle = compose(
                   <div
                     style={{
                       backgroundColor: WHITE,
-                      color: '#212121',
+                      color: "#212121",
                       // backgroundColor: isActive ? BLUE : WHITE,
                       // color: isActive ? WHITE : BLUE,
                       fontSize: 14,
-                      lineHeight: '17px',
-                      padding: '3px 6px',
+                      lineHeight: "17px",
+                      padding: "3px 6px",
                       borderRadius: 8,
-                      position: 'relative',
+                      position: "relative",
                     }}
                     onClick={() => handleSelectDevice(el)}
                     aria-hidden="true"
@@ -115,31 +125,31 @@ const MapGoogle = compose(
                       <Radio
                         checked
                         className={
-                          isOnline ? 'online-device' : 'offline-device'
+                          isOnline ? "online-device" : "offline-device"
                         }
                       />
                       <span style={{ fontWeight: 600 }}>{el.device_name}</span>
-                      <ul style={{ padding: '4px 20px' }}>
+                      <ul style={{ padding: "4px 20px" }}>
                         <span
                           style={{
                             marginLeft: -20,
-                            display: 'flex',
-                            alignItems: 'center',
+                            display: "flex",
+                            alignItems: "center",
                           }}
                         >
                           <HistoryOutlined />
                           &nbsp;
                           {moment
                             .utc(el.lastReceived)
-                            .format('DD/MM/YYYY HH:mm')}
+                            .format("DD/MM/YYYY HH:mm")}
                         </span>
                         <li>
                           <span style={{ fontWeight: 600 }}>Vin</span>
                           :&nbsp;{el.vin} (V)
                         </li>
                         {el.hasPump && (
-                          <li style={{ color: el.status ? '#01bf01' : 'red' }}>
-                            {el.status ? 'Đang kết nối' : 'Mất kết nối'}
+                          <li style={{ color: el.status ? "#01bf01" : "red" }}>
+                            {el.status ? "Đang kết nối" : "Mất kết nối"}
                           </li>
                         )}
                         {!isEmpty(dataInfo) &&
@@ -148,10 +158,10 @@ const MapGoogle = compose(
                               key={i}
                               style={{
                                 color: infoItem.high_warning
-                                  ? '#f44336'
+                                  ? "#f44336"
                                   : infoItem.low_warning
-                                  ? '#fdd835'
-                                  : '#212121',
+                                  ? "#fdd835"
+                                  : "#212121",
                               }}
                             >
                               <span style={{ fontWeight: 600 }}>
@@ -166,7 +176,7 @@ const MapGoogle = compose(
                         type="primary"
                         onClick={(event) => {
                           device_id = el.device_id;
-                          setVisible('view');
+                          setVisible("view");
                           event.stopPropagation();
                         }}
                       >
@@ -179,9 +189,9 @@ const MapGoogle = compose(
                         height: 9,
                         backgroundColor: WHITE,
                         // backgroundColor: isActive ? BLUE : WHITE,
-                        transform: 'rotate(45deg)',
-                        position: 'absolute',
-                        left: 'calc(50% - 5px)',
+                        transform: "rotate(45deg)",
+                        position: "absolute",
+                        left: "calc(50% - 5px)",
                         marginTop: -1,
                       }}
                     />
@@ -191,7 +201,7 @@ const MapGoogle = compose(
             );
           })}
       </GoogleMap>
-      {visible === 'view' && (
+      {visible === "view" && (
         <ViewDeviceModal device_id={device_id} handleClose={handleCloseModal} />
       )}
     </>
